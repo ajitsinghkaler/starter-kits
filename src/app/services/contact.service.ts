7801d5e21a2ac3db70d1efa33e4d940106509da9
Ajit Singh <ajitsinghkaler0@gmail.com>
2024-02-24 19:35:12 +0530

    add removed calls

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -2,7 +2,7 @@ import { DOCUMENT } from '@angular/common';
 import { Injectable, inject } from '@angular/core';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-// declare let Tally: any;
+declare let Tally: any;
 
 @Injectable({
   providedIn: 'root',
@@ -12,55 +12,55 @@ export class ContactService {
   loading = false;
   loaded: Promise<void> | null = null;
 
-  // init(): Promise<void> {
-  //   this.loading = true;
-  //   return this.load().then(() => {
-  //     this.loading = false;
+  init(): Promise<void> {
+    this.loading = true;
+    return this.load().then(() => {
+      this.loading = false;
 
-  //     Tally.loadEmbeds();
-  //   });
-  // }
+      Tally.loadEmbeds();
+    });
+  }
 
-  // private load(): Promise<void> {
-  //   if (!this.loaded) {
-  //     this.loaded = new Promise<void>((resolve, reject) => {
-  //       const script = this.document.createElement('script');
-  //       script.type = 'text/javascript';
-  //       script.src = 'https://tally.so/widgets/embed.js';
+  private load(): Promise<void> {
+    if (!this.loaded) {
+      this.loaded = new Promise<void>((resolve, reject) => {
+        const script = this.document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://tally.so/widgets/embed.js';
 
-  //       // Error event handling with proper type annotation for the event parameter
-  //       script.onerror = (e: Event | string) => {
-  //         reject(e instanceof Event ? e : new Error(e.toString()));
-  //       };
+        // Error event handling with proper type annotation for the event parameter
+        script.onerror = (e: Event | string) => {
+          reject(e instanceof Event ? e : new Error(e.toString()));
+        };
 
-  //       // Extend HTMLScriptElement interface to include readyState for older IE versions compatibility
-  //       interface HTMLScriptElementExtended extends HTMLScriptElement {
-  //         readyState?: 'loaded' | 'complete' | 'uninitialized' | 'loading';
-  //         onreadystatechange?: () => void; // Correctly include onreadystatechange
-  //       }
+        // Extend HTMLScriptElement interface to include readyState for older IE versions compatibility
+        interface HTMLScriptElementExtended extends HTMLScriptElement {
+          readyState?: 'loaded' | 'complete' | 'uninitialized' | 'loading';
+          onreadystatechange?: () => void; // Correctly include onreadystatechange
+        }
 
-  //       const scriptExtended = script as HTMLScriptElementExtended;
+        const scriptExtended = script as HTMLScriptElementExtended;
 
-  //       if (scriptExtended.readyState) {
-  //         scriptExtended.onreadystatechange = () => {
-  //           if (
-  //             scriptExtended.readyState === 'loaded' ||
-  //             scriptExtended.readyState === 'complete'
-  //           ) {
-  //             scriptExtended.onreadystatechange = undefined;
-  //             resolve();
-  //           }
-  //         };
-  //       } else {
-  //         script.onload = () => {
-  //           resolve();
-  //         };
-  //       }
+        if (scriptExtended.readyState) {
+          scriptExtended.onreadystatechange = () => {
+            if (
+              scriptExtended.readyState === 'loaded' ||
+              scriptExtended.readyState === 'complete'
+            ) {
+              scriptExtended.onreadystatechange = undefined;
+              resolve();
+            }
+          };
+        } else {
+          script.onload = () => {
+            resolve();
+          };
+        }
 
-  //       this.document.body.appendChild(script);
-  //     });
-  //   }
+        this.document.body.appendChild(script);
+      });
+    }
 
-  //   return this.loaded;
-  // }
+    return this.loaded;
+  }
 }
