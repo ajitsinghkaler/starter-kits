dc1a956c2e96376b837ab1c3cfc52505bfa31c95
Ajit Singh <ajitsinghkaler0@gmail.com>
2024-02-27 22:49:09 +0530

    add view transitions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import {
   provideRouter,
   withComponentInputBinding,
   withInMemoryScrolling,
+  withViewTransitions,
 } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -14,6 +15,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(
       routes,
+      withViewTransitions(),
       withComponentInputBinding(),
       withInMemoryScrolling({
         anchorScrolling: 'enabled',
